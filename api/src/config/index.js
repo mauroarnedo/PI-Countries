81569f005461c7025240c90318e2b544946c741a
Mauro Arnedo <mauroarnedo@users.noreply.github.com>
2022-11-18 15:53:29 -0300

    Will be ready?

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -1,5 +1,8 @@
-export const PORT = process.env.PORT || 4000;
-export const DB_USER = process.env.DB_USER || "postgres";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "postgres";
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_NAME = process.env.DB_NAME || "countries";
+module.exports = {
+    PORT: process.env.PORT || 4000,
+    DB_PORT: process.env.DB_PORT || 5432,
+    DB_USER: process.env.DB_USER || "postgres",
+    DB_PASSWORD: process.env.DB_PASSWORD || "postgres",
+    DB_HOST: process.env.DB_HOST || "localhost",
+    DB_NAME: process.env.DB_NAME || "countries",
+}
